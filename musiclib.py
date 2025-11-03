9c7133a76ea03d901a10d4bc6ecf0dcd7101a328
Vaibhav Pandey <vaibhavpandeyy2021@gmail.com>
2025-11-03 22:00:51 +0530

    Add comments for song playback instructions
    Added comments for usage instructions and potential expansion.

--- musiclib.py
+++ musiclib.py
@@ -2,3 +2,5 @@ music = {
     "aujla":"https://youtu.be/-YlmnPh-6rE?si=zB-3eHxkGmYKYebi",
     "vikram":"https://youtu.be/tYKrORILFOg?si=xVS6ZnDT6r65I-HF"
 }
+# if u say " play aujla" or "play vikram" the following songs of respective singers will start to play!
+# you can add more links bt defining more key value pairs for the same!
